45b18b5965ed8287eb50767315c00883994f2277
jose d lopez <jose@tumis.com>
2013-01-23 00:35:06 -0800

    skel

--- plugin/uninstall.php
+++ plugin/uninstall.php
@@ -7,4 +7,9 @@
  * The uninstallation script.
  */
 
-if( defined( 'ABSPATH') && defined('WP_UNINSTALL_PLUGIN') ) { }
+if( defined( 'ABSPATH') && defined('WP_UNINSTALL_PLUGIN') ) { 
+
+	//delete fields
+	//delete_option('deportation_clock_data');
+
+}
